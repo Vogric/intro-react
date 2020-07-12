d7a148f68ffb202486e5e9d6336ae16af7ef7e26
Vogrič <53317134+Vogric@users.noreply.github.com>
2020-07-12 18:52:51 -0300

    exercises

---
+++ 02-intro-javascript/src/bases/07-desestructuracion-arr.js
@@ -0,0 +1,27 @@
+//Desestructuracion de arreglos
+const personajes = ["Goku", "Vegeta", "Trunks"];
+const [, , p3] = personajes;
+console.log(p3);
+
+const retornaArreglo = () => {
+  return ["ABC", 123];
+};
+
+const [letras, numeros] = retornaArreglo();
+console.log(letras);
+console.log(numeros);
+
+//Tarea
+
+const useState = (valor) => {
+  return [
+    valor,
+    () => {
+      console.log("Hola mundo");
+    },
+  ];
+};
+
+const [nombre, setNombre] = useState("Goku");
+console.log(nombre);
+setNombre();
