d0b16e281c846a6b7cd9ec8947fa9d7fbb63a33c
Vogrič <53317134+Vogric@users.noreply.github.com>
2020-07-19 11:32:55 -0300

    pruebas

--- 03-counter-app/src/base/05-functions.js
+++ 03-counter-app/src/base/05-functions.js
@@ -4,21 +4,12 @@ const saludar2 = (nombre) => {
 
 const saludar3 = (nombre) => `Hola, ${nombre}`;
 
-console.log(saludar2("Braian"));
-console.log(saludar3("Leonel"));
-
-const getUser = () => ({
+export const getUser = () => ({
   uid: "ABC123",
   username: "wokl31",
 });
 
-const user = getUser();
-console.log(user);
-
-const getUsarioActivo = (nombre) => ({
+export const getUsuarioActivo = (nombre) => ({
   uid: "ABC546",
   username: nombre,
 });
-
-const usuarioActivo = getUsarioActivo("Fernando");
-console.log(usuarioActivo);
