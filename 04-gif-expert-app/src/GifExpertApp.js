384edb01770657f36787535faae4065cee56d490
Vogrič <53317134+Vogric@users.noreply.github.com>
2020-07-20 14:35:24 -0300

    comunicacion entre componentes

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -2,16 +2,12 @@ import React, { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
-  const [categories, setcategories] = useState(["Serie1", "Serie2", "Serie3"]);
-
-  // const handleAdd = () => {
-  //   setcategories([...categories, "Serie4"]);
-  // };
+  const [categories, setCategories] = useState(["Serie1", "Serie2", "Serie3"]);
 
   return (
     <>
       <h2>GifExpertApp</h2>
-      <AddCategory setcategories={setcategories} />
+      <AddCategory setCategories={setCategories} />
       <hr />
       <ol>
         {categories.map((category) => {
